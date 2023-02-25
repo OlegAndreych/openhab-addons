d36c784208d1f678d3929f931b17f9887c89e9e6
Oleg Andreych <kjiec4@gmail.com>
2023-02-26 00:32:29 +0500

    * Implementing discovery;

---
+++ bundles/org.openhab.binding.qingping/src/main/java/org/openhab/binding/qingping/internal/discovery/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Means for automatic discovery of things.
+ */
+@NonNullByDefault
+package org.openhab.binding.qingping.internal.discovery;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
---
+++ bundles/org.openhab.binding.qingping/src/main/java/org/openhab/binding/qingping/internal/sync/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Means to sync devices' state from the Qingping cloud.
+ */
+@NonNullByDefault
+package org.openhab.binding.qingping.internal.sync;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
